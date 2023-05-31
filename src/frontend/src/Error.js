d2e6130f6cb50e9ad993788550588cf2fb5bc3f0
Vineet <vkachha@amazon.com>
2023-05-31 13:54:08 -0400

    Auth error handling for different user selfie provided

--- src/frontend/src/Error.js
+++ src/frontend/src/Error.js
@@ -1,9 +1,10 @@
 const ErrorMessage = {
- 'UserAlreadyExists' : 'The user already exists. Delete the user first to try out this feature.',
- 'FaceNotMatchWithIDCard':'The image from the camera and ID card don’t match. Please use your valid ID documents.',
-'GenericError': 'An error occurred while submitting the form. Please try again later.',
-'ValueError': 'The user does not exist. Please register the user first.',
-'LiveNessResultThreshold':'The Face Liveness confidence score is less than the user-selected confidence threshold. Please reattempt the face liveness check again.'
+    'UserAlreadyExists': 'The user already exists. Delete the user first to try out this feature.',
+    'FaceNotMatchWithIDCard': 'The image from the camera and ID card don’t match. Please use your valid ID documents.',
+    'GenericError': 'An error occurred while submitting the form. Please try again later.',
+    'ValueError': 'The user does not exist. Please register the user first.',
+    'LiveNessResultThreshold': 'The Face Liveness confidence score is less than the user-selected confidence threshold. Please reattempt the face liveness check again.',
+    'UserAccessDenied': 'Access Denied. The wrong person is in the photo.'
 }
 
 export default ErrorMessage;
